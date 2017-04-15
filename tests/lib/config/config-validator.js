126580713f6f00a8e0b89618b914d5de5bba7cfb
Gyandeep Singh <gyandeeps@gmail.com>
2017-04-15 12:35:56 -0500

    New: Immutable Linter API

--- tests/lib/config/config-validator.js
+++ tests/lib/config/config-validator.js
@@ -10,8 +10,9 @@
 //------------------------------------------------------------------------------
 
 const assert = require("chai").assert,
-    eslint = require("../../../lib/eslint"),
+    Linter = require("../../../lib/Linter"),
     validator = require("../../../lib/config/config-validator");
+const linter = new Linter();
 
 //------------------------------------------------------------------------------
 // Tests
@@ -90,126 +91,126 @@ const mockRequiredOptionsRule = {
 describe("Validator", () => {
 
     beforeEach(() => {
-        eslint.defineRule("mock-rule", mockRule);
-        eslint.defineRule("mock-required-options-rule", mockRequiredOptionsRule);
+        linter.defineRule("mock-rule", mockRule);
+        linter.defineRule("mock-required-options-rule", mockRequiredOptionsRule);
     });
 
     describe("validate", () => {
 
         it("should do nothing with an empty config", () => {
-            const fn = validator.validate.bind(null, {}, "tests");
+            const fn = validator.validate.bind(null, {}, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with an empty rules object", () => {
-            const fn = validator.validate.bind(null, { rules: {} }, "tests");
+            const fn = validator.validate.bind(null, { rules: {} }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with a valid config", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-rule": [2, "second"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-rule": [2, "second"] } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with a valid config when severity is off", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["off", "second"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["off", "second"] } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with an invalid config when severity is off", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-required-options-rule": "off" } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-required-options-rule": "off" } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with an invalid config when severity is an array with 'off'", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-required-options-rule": ["off"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-required-options-rule": ["off"] } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with a valid config when severity is warn", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["warn", "second"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["warn", "second"] } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with a valid config when severity is error", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["error", "second"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["error", "second"] } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with a valid config when severity is Off", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["Off", "second"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["Off", "second"] } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with a valid config when severity is Warn", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["Warn", "second"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["Warn", "second"] } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should do nothing with a valid config when severity is Error", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["Error", "second"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-rule": ["Error", "second"] } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should catch invalid rule options", () => {
-            const fn = validator.validate.bind(null, { rules: { "mock-rule": [3, "third"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-rule": [3, "third"] } }, "tests", linter.rules, linter.environments);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"mock-rule\" is invalid:\n\tSeverity should be one of the following: 0 = off, 1 = warn, 2 = error (you passed '3').\n");
         });
 
         it("should allow for rules with no options", () => {
-            eslint.defineRule("mock-no-options-rule", mockNoOptionsRule);
+            linter.defineRule("mock-no-options-rule", mockNoOptionsRule);
 
-            const fn = validator.validate.bind(null, { rules: { "mock-no-options-rule": 2 } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-no-options-rule": 2 } }, "tests", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
 
         it("should not allow options for rules with no options", () => {
-            eslint.defineRule("mock-no-options-rule", mockNoOptionsRule);
+            linter.defineRule("mock-no-options-rule", mockNoOptionsRule);
 
-            const fn = validator.validate.bind(null, { rules: { "mock-no-options-rule": [2, "extra"] } }, "tests");
+            const fn = validator.validate.bind(null, { rules: { "mock-no-options-rule": [2, "extra"] } }, "tests", linter.rules, linter.environments);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"mock-no-options-rule\" is invalid:\n\tValue \"extra\" has more items than allowed.\n");
         });
 
         it("should throw with an array environment", () => {
-            const fn = validator.validate.bind(null, { env: [] });
+            const fn = validator.validate.bind(null, { env: [] }, "", linter.rules, linter.environments);
 
             assert.throws(fn, "Environment must not be an array");
         });
 
         it("should throw with a primitive environment", () => {
-            const fn = validator.validate.bind(null, { env: 1 });
+            const fn = validator.validate.bind(null, { env: 1 }, "", linter.rules, linter.environments);
 
             assert.throws(fn, "Environment must be an object");
         });
 
         it("should catch invalid environments", () => {
-            const fn = validator.validate.bind(null, { env: { browser: true, invalid: true } });
+            const fn = validator.validate.bind(null, { env: { browser: true, invalid: true } }, "", linter.rules, linter.environments);
 
             assert.throws(fn, "Environment key \"invalid\" is unknown\n");
         });
 
         it("should catch disabled invalid environments", () => {
-            const fn = validator.validate.bind(null, { env: { browser: true, invalid: false } });
+            const fn = validator.validate.bind(null, { env: { browser: true, invalid: false } }, "", linter.rules, linter.environments);
 
             assert.throws(fn, "Environment key \"invalid\" is unknown\n");
         });
 
         it("should do nothing with an undefined environment", () => {
-            const fn = validator.validate.bind(null, {});
+            const fn = validator.validate.bind(null, {}, "", linter.rules, linter.environments);
 
             assert.doesNotThrow(fn);
         });
@@ -219,12 +220,12 @@ describe("Validator", () => {
     describe("getRuleOptionsSchema", () => {
 
         it("should return null for a missing rule", () => {
-            assert.equal(validator.getRuleOptionsSchema("non-existent-rule"), null);
+            assert.equal(validator.getRuleOptionsSchema("non-existent-rule", linter.rules), null);
         });
 
         it("should not modify object schema", () => {
-            eslint.defineRule("mock-object-rule", mockObjectRule);
-            assert.deepEqual(validator.getRuleOptionsSchema("mock-object-rule"), {
+            linter.defineRule("mock-object-rule", mockObjectRule);
+            assert.deepEqual(validator.getRuleOptionsSchema("mock-object-rule", linter.rules), {
                 enum: ["first", "second"]
             });
         });
@@ -234,43 +235,43 @@ describe("Validator", () => {
     describe("validateRuleOptions", () => {
 
         it("should throw for incorrect warning level number", () => {
-            const fn = validator.validateRuleOptions.bind(null, "mock-rule", 3, "tests");
+            const fn = validator.validateRuleOptions.bind(null, "mock-rule", 3, "tests", linter.rules);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"mock-rule\" is invalid:\n\tSeverity should be one of the following: 0 = off, 1 = warn, 2 = error (you passed '3').\n");
         });
 
         it("should throw for incorrect warning level string", () => {
-            const fn = validator.validateRuleOptions.bind(null, "mock-rule", "booya", "tests");
+            const fn = validator.validateRuleOptions.bind(null, "mock-rule", "booya", "tests", linter.rules);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"mock-rule\" is invalid:\n\tSeverity should be one of the following: 0 = off, 1 = warn, 2 = error (you passed '\"booya\"').\n");
         });
 
         it("should throw for invalid-type warning level", () => {
-            const fn = validator.validateRuleOptions.bind(null, "mock-rule", [["error"]], "tests");
+            const fn = validator.validateRuleOptions.bind(null, "mock-rule", [["error"]], "tests", linter.rules);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"mock-rule\" is invalid:\n\tSeverity should be one of the following: 0 = off, 1 = warn, 2 = error (you passed '[ \"error\" ]').\n");
         });
 
         it("should only check warning level for nonexistent rules", () => {
-            const fn = validator.validateRuleOptions.bind(null, "non-existent-rule", [3, "foobar"], "tests");
+            const fn = validator.validateRuleOptions.bind(null, "non-existent-rule", [3, "foobar"], "tests", linter.rules);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"non-existent-rule\" is invalid:\n\tSeverity should be one of the following: 0 = off, 1 = warn, 2 = error (you passed '3').\n");
         });
 
         it("should only check warning level for plugin rules", () => {
-            const fn = validator.validateRuleOptions.bind(null, "plugin/rule", 3, "tests");
+            const fn = validator.validateRuleOptions.bind(null, "plugin/rule", 3, "tests", linter.rules);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"plugin/rule\" is invalid:\n\tSeverity should be one of the following: 0 = off, 1 = warn, 2 = error (you passed '3').\n");
         });
 
         it("should throw for incorrect configuration values", () => {
-            const fn = validator.validateRuleOptions.bind(null, "mock-rule", [2, "frist"], "tests");
+            const fn = validator.validateRuleOptions.bind(null, "mock-rule", [2, "frist"], "tests", linter.rules);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"mock-rule\" is invalid:\n\tValue \"frist\" must be an enum value.\n");
         });
 
         it("should throw for too many configuration values", () => {
-            const fn = validator.validateRuleOptions.bind(null, "mock-rule", [2, "first", "second"], "tests");
+            const fn = validator.validateRuleOptions.bind(null, "mock-rule", [2, "first", "second"], "tests", linter.rules);
 
             assert.throws(fn, "tests:\n\tConfiguration for rule \"mock-rule\" is invalid:\n\tValue \"first,second\" has more items than allowed.\n");
         });
